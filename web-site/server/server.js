81d5a9a34d545ce3ae437d88898497f010634974
juan-gon <agonzales@peruyoung.com>
2020-09-02 16:06:58 +0200

    ajustes

--- web-site/server/server.js
+++ web-site/server/server.js
@@ -11,10 +11,6 @@ app.use(bodyParser.urlencoded({ extends: false }));
 app.use(bodyParser.json());
 //
 
-app.get('/usuario', function(req, res) {
-    res.json('get usaurio');
-});
-
 app.post('/usuario', function(req, res) {
     let body = req.body;
     if (body.nombre === undefined) {
@@ -25,6 +21,44 @@ app.post('/usuario', function(req, res) {
     } else {
         res.json({
             body
+        });
+    }
+});
+
+app.get('/usuario', function(req, res) {
+    console.log(req);
+    let body = req.body;
+    returnString = '';
+
+    airline = body.airline;
+    flightNum = body.flightNumber;
+    fullQual = airline + flightNum;
+
+    if (body.airline === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'Acceso denegado'
+        });
+    } else {
+        switch (fullQual) {
+            case 'AA456':
+                returnString = 'AA456 is 10 minutes ahead of schedule.';
+                break;
+            case 'AA123':
+                returnString = 'AA123 is on time.';
+                break;
+            case 'DL123':
+                returnString = 'DL123 is on time.';
+                break;
+            case 'UA789':
+                returnString = 'UA789 is 5 minutes behind schedule.';
+                break;
+            default:
+                returnString = '**An error has occured.**';
+        }
+        let finalString = " --> " + returnString + " <-- ";
+        res.json({
+            message: finalString
         });
     }
 });
@@ -42,4 +76,34 @@ app.delete('/usuario', function(req, res) {
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto ${process.env.PORT}`);
-})
+});
+
+function manti(res) {
+    returnString = '';
+
+    airline = res.airline;
+    flightNum = res.flightNumber;
+    fullQual = airline + flightNum;
+
+    // This section could be an API call, database call, etc.
+    switch (fullQual) {
+        case 'AA456':
+            returnString = 'AA456 is 10 minutes ahead of schedule.';
+            break;
+        case 'AA123':
+            returnString = 'AA123 is on time.';
+            break;
+        case 'DL123':
+            returnString = 'DL123 is on time.';
+            break;
+        case 'UA789':
+            returnString = 'UA789 is 5 minutes behind schedule.';
+            break;
+        default:
+            returnString = '**An error has occured.**';
+    }
+    //
+
+    let finalString = " --> " + returnString + " <-- ";
+    return { message: finalString };
+}
